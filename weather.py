b5f4b03cabf4c8b9165587aefcee104146034bdf
Z-Ferguson <zfergusonn@gmail.com>
2016-11-28 15:47:04 -0500

    have API endpoints printing correctly

--- weather.py
+++ weather.py
@@ -8,76 +8,71 @@ import os.path
 class CurrentCondition:
     def __init__(self, zipcode):
         self.zipcode = zipcode
-        self.url = http://api.wunderground.com/api/103c92d6094529b8/conditions/q/{}{}.format(self.zipcode, .json)
+        self.url = "http://api.wunderground.com/api/103c92d6094529b8/conditions/q/{}{}".format(self.zipcode, '.json')
 
-    def get_current_condition():
+    def get_current_condition(self):
         r = requests.get(self.url)
-        json.dumps(results, indent=4))
+        print(json.dumps(r.json(), indent=4))
 
+
+cc = CurrentCondition(27713)
+cc.get_current_condition()
 
 # 10 day forecast for that location
 
 
 class TenDayForecast:
-    pass
+    def __init__(self, zipcode):
+        self.zipcode = zipcode
+        self.url = "http://api.wunderground.com/api/103c92d6094529b8/forecast10day/q/{}{}".format(self.zipcode, '.json')
 
     def get_ten_day_forecast():
-        pass
+        r = requests.get(self.url)
+        print(json.dumps(r.json(), indent=4))
 
 # Sunrise and sunset times
 
 
 class SunriseSunset:
-    pass
+    def __init__(self, zipcode):
+        self.zipcode = zipcode
+        self.url = "http://api.wunderground.com/api/103c92d6094529b8/astronomy/q/{}{}".format(self.zipcode, '.json')
 
     def get_sunrise_sunset():
-        pass
+        r = requests.get(self.url)
+        print(json.dumps(r.json(), indent=4))
+
+
+    # sr = SunriseSunset(27713)
+    # sr.SunriseSunset()
+
 
 # Any current weather alerts
 
 
 class WeatherAlert:
-    pass
+    def __init__(self, zipcode):
+        self.zipcode = zipcode
+        self.url = (
+                    "http://api.wunderground.com/api/103c92d6094529b8/alerts/q/
+                    {}{}".format(self.zipcode, '.json')
+        )
 
     def get_weather_alert():
-        pass
+        r = requests.get(self.url)
+        print(json.dumps(r.json(), indent=4))
 
 # A list of all active hurricanes (anywhere)
 
 
 class Hurricane:
-    pass
+    def __init__(self, zipcode):
+        self.zipcode = zipcode
+        self.url = (
+        "http://api.wunderground.com/api/103c92d6094529b8/currenthurricane/q/{}{}".format(self.zipcode, '.json')
 
     def get_hurricanes():
-        pass
-
-
-def get_weather_info():
-    filename = "wunder_cache_weather.json"
-    if os.path.isfile(filename):
-        with open(filename, "r") as f:
-            results = json.loads(f.read())
-    else:
-        url = "http://api.wunderground.com/api/"
-        r = requests.get(url)
-        results = r.json()[" "]
-
-        while r.json()[" "]:
-            r = requests.get(r.json()[" "])
-            results += r.json()[" "]
-
-        with open(filename, "w") as f:
-            f.write(json.dumps(results, indent=4))
-
-    return results
-
-
-def find_zipcode(where, weather):
-    for person in weather:
-        if person[" "].lower() == where.lower():
-            print(person)
-
+        r = requests.get(self.url)
+        print(json.dumps(r.json(), indent=4))
 
 if __name__ == "__main__":
-    weather = get_weather_info()
-    find_zipcode(input("where would you like to look up? (zipcode) "), weather)
